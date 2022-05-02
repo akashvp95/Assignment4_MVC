e4f8106e67197573fa46810326cc40018bbb11fb
akashvp95 <98869796+akashvp95@users.noreply.github.com>
2022-05-02 14:05:19 +0530

    Add files via upload

---
+++ Accounts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment4.Models
+{
+    public class Accounts
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+        public double CurrentBalance { get; set; }
+
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment4.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Assignment4.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDbContext context = new MyDbContext();
+        public ActionResult Index()
+        {
+            return View(context.Accounts);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult CreateAccount(Accounts a)
+        {
+            context.Accounts.Add(a);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? accno)
+        {
+            var account_to_edit = (from a in context.Accounts
+                                   where a.AccountNumber == accno
+                                   select a).SingleOrDefault();
+            return View(account_to_edit);
+        }
+
+        public ActionResult EditAccount(Accounts a)
+        {
+            context.Entry<Accounts>(a).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int? accno)
+        {
+            var account_to_delete = (from a in context.Accounts
+                                     where a.AccountNumber == accno
+                                     select a).SingleOrDefault();
+            context.Entry<Accounts>(account_to_delete).State =
+                    System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetNews(string category)
+        {
+            return PartialView(null, category);
+        }
+
+    }
+}
